1d75851417cf2691bb9422885e81a44a5a75eea4
jxhuangphys <36110270+jxhuangphys@users.noreply.github.com>
2024-11-17 16:14:00 -0800

    Add files via upload

--- pbc_relocate.m
+++ pbc_relocate.m
@@ -1,13 +1,24 @@
 function points = pbc_relocate(center, points, box_size)
+% Function to relocate points to their periodic boundary conditions (PBC)
+% relative to a given center. Ensures that points are confined within a
+% simulation box defined by box_size and are close to the center.
 
+% Iterate over each point in the points array
 for i = 1:size(points, 1)
+    % Extract the current point coordinates
     i_point = points(i, :);
+    
+    % Calculate the relative difference between the point and the center
+    % Normalize this difference by dividing by the box size and shift by 0.5
+    % to center it in the range [0, 1].
     difference = (i_point - center)./box_size + 0.5;
-     
-    % we want this difference to be between 0 and 1
-    % because (difference - 0.5) should be between -0.5 and 0.5
+    
+    % Adjust the point coordinates to fall within the box boundaries.
+    % By taking the floor of the normalized difference, we determine the
+    % number of box lengths to subtract from the point.
+    % This ensures the final point remains within the range [0, box_size].
     points(i, :) = i_point - floor(difference).*box_size;
 end
- 
 
-end % end of function
+% End of function
+end
